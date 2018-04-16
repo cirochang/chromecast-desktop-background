ef84e27477403d92ee083859b0b799db95808cb9
Ciro Chang <ciro.chang@tvglobo.com.br>
2018-04-15 23:50:47 -0300

    remove recursion limit memory error

--- main.py
+++ main.py
@@ -15,27 +15,31 @@ def init_browser():
     return webdriver.Firefox(executable_path=GECKODRIVER_PATH)
 
 def go_to_chromecast_page(browser):
-    try:
-        browser.get("https://clients3.google.com/cast/chromecast/home")
-    except:
-        go_to_chromecast_page()
+    connected = False
+    while(!connected)
+        try:
+            browser.get("https://clients3.google.com/cast/chromecast/home")
+            connected = True
+        except:
+            time.sleep(2)
+
 
 def set_background_img_loop(browser, last_img_uri=""):
-    try:
-        img_uri = browser.find_element_by_id("picture-background").get_attribute("src")
-        if(img_uri != last_img_uri):
-            last_img_uri = img_uri
-            # save the img
-            f = urllib.urlopen(last_img_uri)
-            with open(TEMP_IMAGE_PATH, "wb") as imgFile:
-                imgFile.write(f.read())
-            # move the file temp_img to img
-            os.system("mv %s %s 2>/dev/null" % (TEMP_IMAGE_PATH, IMAGE_PATH))
-            # set background desktop
-            os.system('gsettings set org.gnome.desktop.background picture-uri "file://'+IMAGE_PATH+'"')
-    finally:
-        time.sleep(2)
-        set_background_img_loop(browser, last_img_uri)
+    while(True)
+        try:
+            img_uri = browser.find_element_by_id("picture-background").get_attribute("src")
+            if(img_uri != last_img_uri):
+                last_img_uri = img_uri
+                # save the img
+                f = urllib.urlopen(last_img_uri)
+                with open(TEMP_IMAGE_PATH, "wb") as imgFile:
+                    imgFile.write(f.read())
+                # move the file temp_img to img
+                os.system("mv %s %s 2>/dev/null" % (TEMP_IMAGE_PATH, IMAGE_PATH))
+                # set background desktop
+                os.system('gsettings set org.gnome.desktop.background picture-uri "file://'+IMAGE_PATH+'"')
+        finally:
+            time.sleep(2)
 
 if __name__ == "__main__":
     print("Initializing browser in background...")
